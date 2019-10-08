496afe523afb4c19e02b8cd8cf2735eaaa3e485f
chrstphrbtchr <chrstphrbtchr@users.noreply.github.com>
2019-10-07 20:56:56 -0400

    finishing it up now!

---
+++ tiles_spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tiles_spritesheet" tilewidth="32" tileheight="32" tilecount="812" columns="28">
+ <image source="assets/platformerGraphicsDeluxe_Updated/Tiles/tiles_spritesheet.png" width="914" height="936"/>
+</tileset>
